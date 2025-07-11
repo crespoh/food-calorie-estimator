7b3ea0b81fdcf1783e4880c1eff18653f249dbc2
crespoh <crespoh@gmail.com>
2025-07-11 21:08:39 +0800

    separate client and server

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  publicDir: false,
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
   build: {
-    outDir: 'public',
+    outDir: path.resolve(__dirname, "server/public"),
     emptyOutDir: true,
   },
   server: {
